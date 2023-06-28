543dd84b73187c086f4b9e0cbc7581a2436e80b6
umberto.salvai <umberto.salvai@edu.itspiemonte.it>
2023-06-28 10:44:35 -0500

    inserimento transazioni

--- Storico_Transazioni.py
+++ Storico_Transazioni.py
@@ -5,7 +5,7 @@ import sqlite3
 def StoricoTransazioni(nome,cognome):
     conn = sqlite3.connect('PYTHON.db')
     cursor = conn.cursor()
-    
+
     transazioniPage = tk.Tk()
     transazioniPage.title("MENU'")
     transazioniPage.geometry("600x600")
@@ -23,11 +23,17 @@ def StoricoTransazioni(nome,cognome):
             "bd": 0,
         }
     
-
-    select_query = "SELECT * FROM UTENTI WHERE nome = ? and cognome = ? "
+    def avvia_ricerca():
+        result = ricerca_Transazioni(Id)
+        if result:
+            print(result)  # Stampa la riga restituita dalla query
+        else:
+            print("Nessun risultato trovato.")
+    select_query = "SELECT IDutenti FROM UTENTI WHERE nome = ? and cognome = ? "
     cursor.execute(select_query, (nome,cognome))
-
-    avvia_ricerca_button = tk.Button(transazioniPage, command=ricerca_Transazioni(Id),text="Avvia la ricerca", **button_style)
+    Id = cursor.fetchone()
+    
+    avvia_ricerca_button = tk.Button(transazioniPage, command=avvia_ricerca(),text="Avvia la ricerca", **button_style)
     titolo_label = tk.Label(parte_superiore, text="THE BANK", font=("Arial", 30, "bold", "italic"), bg="#FFFFFF")
    
     avvia_ricerca_button.pack(pady=(320, 0))
@@ -41,13 +47,10 @@ def ricerca_Transazioni(Id):
     cursor = conn.cursor()
     
     # Esegui la query SELECT per verificare se il nome esiste già
-    select_query = "SELECT * FROM UTENTI WHERE nome = ? and cognome = ? "
-    cursor.execute(select_query, (nome,cognome))
+    select_query = "SELECT * FROM ContoCorrente WHERE IDutente==?"
+    cursor.execute(select_query, (Id))
     
-    # Ottieni il risultato della query
     result = cursor.fetchone()
-    
-    # Chiudi la connessione al database
     cursor.close()
     conn.close()
     
--- prova.py
+++ prova.py
@@ -1,7 +1,44 @@
 import tkinter as tk
-from tkinter import messagebox
+import sqlite3
+
+def ricerca_Transazioni(Id):
+    conn = sqlite3.connect('PYTHON.db')
+    cursor = conn.cursor()
+
+    # Esegui la query SELECT per ottenere le transazioni dell'utente
+    select_query = "SELECT * FROM ContoCorrente WHERE IDutente=?"
+    cursor.execute(select_query, (Id,))
+
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    # Restituisci la lista di righe restituite dalla query
+    return result
 
 
+def avvia_ricerca():
+    conn = sqlite3.connect('PYTHON.db')
+    cursor = conn.cursor()
+    text_area = tk.Text(transazioniPage, font=("Arial", 12))
+    text_area.pack(pady=(100, 0))
+
+    select_query = "SELECT IDutenti FROM UTENTI WHERE nome = ? and cognome = ?"
+    cursor.execute(select_query, ('io', 'io'))
+    Id = cursor.fetchone()
+
+    if Id:
+        result = ricerca_Transazioni(Id[0])
+        if result:
+            for row in result:
+                text_area.insert(tk.END, str(row) + "\n")  # Aggiungi ogni riga alla widget di testo
+        else:
+            text_area.insert(tk.END, "Nessun risultato trovato.\n")
+    else:
+        text_area.insert(tk.END, "Nessun ID utente trovato.\n")
+
+    cursor.close()
+    conn.close()
 
 
 transazioniPage = tk.Tk()
@@ -9,12 +46,8 @@ transazioniPage.title("MENU'")
 transazioniPage.geometry("600x600")
 transazioniPage.resizable(False, False)
 transazioniPage.configure(background="#00008B")
-
 parte_superiore = tk.Frame(transazioniPage, bg="#FFFFFF")
 parte_superiore.place(relx=0, rely=0, relwidth=1, relheight=0.2)
-
-titolo_label = tk.Label(parte_superiore, text="THE BANK", font=("Arial", 30, "bold", "italic"), bg="#FFFFFF")
-titolo_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 button_style = {
     "font": ("Arial", 14),
     "width": 15,
@@ -25,9 +58,12 @@ button_style = {
     "bd": 0,
 }
 
-avvia_ricerca_button = tk.Button(transazioniPage, command=,text="Avvia la ricerca", **button_style)
 
+
+avvia_ricerca_button = tk.Button(transazioniPage, command=avvia_ricerca, text="Avvia la ricerca", **button_style)
+titolo_label = tk.Label(parte_superiore, text="THE BANK", font=("Arial", 30, "bold", "italic"), bg="#FFFFFF")
 
 avvia_ricerca_button.pack(pady=(320, 0))
+titolo_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 transazioniPage.mainloop()
